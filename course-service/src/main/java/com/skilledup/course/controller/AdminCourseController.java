5c851d35f9b45424b49a867fe15d3c3eab275ce9
rishiwork16-sys <rishiwork16@gmail.com>
2026-02-28 14:00:08 +0530

    Fix: Remove duplicate @CrossOrigin to resolve CORS header conflict

--- course-service/src/main/java/com/skilledup/course/controller/AdminCourseController.java
+++ course-service/src/main/java/com/skilledup/course/controller/AdminCourseController.java
@@ -15,7 +15,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/admin/courses")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AdminCourseController {
 
     @Autowired
