8197e1c745ccf06cb9196fdbbfe33e9dc82c5e7e
rishiwork16-sys <rishiwork16@gmail.com>
2026-02-28 13:45:57 +0530

    Fix: Add @CrossOrigin to AdminCourseController to resolve multipart upload CORS issues

--- course-service/src/main/java/com/skilledup/course/controller/AdminCourseController.java
+++ course-service/src/main/java/com/skilledup/course/controller/AdminCourseController.java
@@ -15,6 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/admin/courses")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AdminCourseController {
 
     @Autowired
